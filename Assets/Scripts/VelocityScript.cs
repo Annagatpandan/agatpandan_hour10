3b4c46d2e6c24475bb1b8df5da268e074cf38aa0
Annagatpandan <kissyndelilah6012@gmail.com>
2023-04-13 19:08:30 -0700

    Changed the position of the 4th wall

--- Assets/Scripts/VelocityScript.cs
+++ Assets/Scripts/VelocityScript.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using Unity Engine;
 
-public class NewBehaviourScript : MonoBehaviour
+
+public class VelocityScript: MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {
-        
+    { 
+    }
+        public float startSpeed = 50f;
     }
 
-    // Update is called once per frame
+    // Update is called once per frame ;
     void Update()
     {
-        
-    }
-}
+       Rigidbody rigidbody = GetComponent<RigidBody>();
+    }  rigidBody.velocity = new Vector3("startSpeed, 0, startSpeed");
+} 
+
